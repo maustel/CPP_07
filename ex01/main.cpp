f07b05de8362273f34513a5b4b76968071f8d784
Milena Austel <maustel@2-E-9.42heilbronn.de>
2025-04-22 15:53:14 +0200

    ex 00 and ex01 done. ex02 still in work

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -11,22 +11,23 @@
 /* ************************************************************************** */
 
 #include "iter.hpp"
+#include "colors.h"
 
 int main()
 {
 	int arr[3] = {1, 2, 3};
 	iter(arr, 3, increase);
-	std::cout << "array content after increase:\n" << arr[0] << std::endl << arr[1] << std::endl << arr[2] << std::endl ;
+	std::cout << MAG << "array content after increase:\n" << RESET << arr[0] << std::endl << arr[1] << std::endl << arr[2] << std::endl ;
 	iter(arr, 3, decrease);
-	std::cout << "array content after decrease:\n" << arr[0] << std::endl << arr[1] << std::endl << arr[2] << std::endl ;
+	std::cout << MAG << "array content after decrease:\n" << RESET << arr[0] << std::endl << arr[1] << std::endl << arr[2] << std::endl ;
 
 	double arrd[3] = {1.1, 2.2, 3.3};
 	iter (arrd, 3, increase);
-	std::cout << "double array content after increase:\n" << arrd[0] << std::endl << arrd[1] << std::endl << arrd[2] << std::endl ;
+	std::cout << MAG << "double array content after increase:\n" << RESET << arrd[0] << std::endl << arrd[1] << std::endl << arrd[2] << std::endl ;
 
 	char str[3] = {'a', 'b', 'c'};
 	iter(str, 3, increase);
-	std::cout << "str after increase:\n" << str << std::endl;
-	
+	std::cout << MAG << "str after increase:\n" << RESET << str << std::endl;
+
 	return (0);
 }
